9c64b3f73953ee34d18b9fde50fce9ba2ed43d54
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-11-10 17:22:48 -0800

    I don't really know that I added ANYTHING in this commit, but I'm about to leave this house/computer so whatevs

--- organism.py
+++ organism.py
@@ -1,10 +1,23 @@
 from simplejson import load
 import typeMatrix
 
+
+
+
 class organism:
+  typeCost = 64
   def __init__(self, jsonName):
+    self.points = 0
     self._data = load(open(jsonName + ".json"))
+    hasType = False
     self.types = (self._data["types"].split(" "))
     self.typekey = typeMatrix.check(0)
     for t in self.types:
+      if not hasType:
+        hasType = True
+      else:
+        self.points += self.typeCost
       self.typekey |= typeMatrix.check(t)
+    for s in self._data["stats"]:
+      pass #do some stuff
+
