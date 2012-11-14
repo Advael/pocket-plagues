5412822a9472cb44b74db0a7508eb5e8a73074c7
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-11-13 20:13:42 -0800

    simplified this considerably

--- typeMatrix.py
+++ typeMatrix.py
@@ -1,54 +1,25 @@
-from bitstring import Bits as bits
 from simplejson import load
+from fuzzywuzzy import process
+from fractions import Fraction
 
 __data = load(open("typeMatrix.json"))
-
 matrix = __data["matrix"]
 names = __data["types"]
-bitkeys = {}
+del __data
 
-for k in names:
-  names[k] = {"n":names[k]}
-  names[k]["bitkey"] = bits("uint:16="+str(2**names[k]["n"])) 
-  names[k]["offense"] = matrix[names[k]["n"]]
-  names[k]["defense"] = [a[names[k]["n"]] for a in matrix] 
-  bitkeys[names[k]["bitkey"]] = {"name":k}
-for k in bitkeys:
-  bitkeys[k]["offense"] = names[bitkeys[k]["name"]]["offense"]
-  bitkeys[k]["defense"] = names[bitkeys[k]["name"]]["defense"]
-
-def check(key):
-  """
-    (valid type name or bitstring,
-     string, int, or hex to use as bitstring)
-      --> bitstring
-  """
-  if type(key) == type(bits()):
-    return key
-  elif type(key) == type(str()):
-    if key.title() in names:
-      return names[key.title()]["bitkey"]
-    else:
-      return bits(key)
-  elif type(key) == type(int()):
-    return bits("uint:16=" + str(key))
-  elif type(key) == type(hex()):
-    return bits("hex:16=" + str(key))
-  else:
-    raise TypeError('Expected a bitstring')
-  
+def check(orgType):
+  r = process.extractOne(orgType, names)
+  assert r[1] > 90, "Ambiguous or invalid type."
+  return r[0]
 
 def matchup(offense, defense):
   """
-    (offensive type bitstring, defensive type(s) bitstring)
-      --> (numerator, denominator) for damage
+  (string, list of string) -> Fraction
   """
-  num = 1
-  denom = 1
-  offense = check(offense)
-  defense = check(defense)[::-1]
-  for i in range(0, 16):
-    if defense[i]:
-      num *= bitkeys[offense]["offense"][i]
-      denom *= 2
-  return (num, denom)
+  n = 1
+  d = 1
+  o = matrix[names.index(check(offense))]
+  for i in defense:
+    n *= o[names.index(check(i))]
+    d *= 2
+  return Fraction(n, d)
