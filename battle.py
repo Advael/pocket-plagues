1b61b438d0c5f62908d7acf658d180fb1568eccd
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-11-14 00:10:44 -0800

    basically a placeholder

--- battle.py
+++ battle.py
@@ -1,15 +1,23 @@
 from organism import organism
+from trainer import trainer
 import control
 
 class battle:
   def __init__(self, one, two):
-    for i in [one, two]:
+    self.one, self.two = one, two
+    for i in [self.one, self.two]:
       if type(i) == type(organism):
         i.setController(control.dumb)
       elif type(i) == type(trainer):
         i.setController(control.basicPlayer)
   def turn(self):
-    pass
+    phaseTrainers()
+    
 
-    #Do trainer actions simultaneously
+    #Do trainer phase simultaneously
+      #No part of trainer phase can affect
     #Do organism actions in descending order of speed
+  def phaseTrainers(self):
+    pass
+  def phase(self, organism):
+
