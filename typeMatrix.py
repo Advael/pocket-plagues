ca111162d2576bd89942e7fef72dffb4faafd73e
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-11-13 20:47:56 -0800

    there, simplified this a bit more

--- typeMatrix.py
+++ typeMatrix.py
@@ -4,22 +4,19 @@ from fractions import Fraction
 
 __data = load(open("typeMatrix.json"))
 matrix = __data["matrix"]
-names = __data["types"]
+types = __data["types"]
 del __data
 
 def check(orgType):
-  r = process.extractOne(orgType, names)
+  r = process.extractOne(orgType, types)
   assert r[1] > 90, "Ambiguous or invalid type."
   return r[0]
 
 def matchup(offense, defense):
-  """
-  (string, list of string) -> Fraction
-  """
   n = 1
   d = 1
-  o = matrix[names.index(check(offense))]
+  o = matrix[types[check(offense)]]
   for i in defense:
-    n *= o[names.index(check(i))]
+    n *= o[types[check(i)]]
     d *= 2
   return Fraction(n, d)
