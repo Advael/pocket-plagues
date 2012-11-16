005cf10c60b2930386dbc8c8a291de0ef1e4f21c
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-11-15 21:33:28 -0800

    damage calculation for attack and specials

--- moves.py
+++ moves.py
@@ -2,6 +2,8 @@ from fuzzywuzzy import process, fuzz
 from essences import check
 from errors import ExceedError
 from rules import *
+from essences import matchup
+from fractions import Fraction
 
 
 class _move:
@@ -11,12 +13,29 @@ class _move:
     if fuzz.ratio(self.mode, "Trainer") > 90:
       pass
     else:
-      if process.extract(self.mode, ["Attack", "Special"])[1] > 100:
+      if self.mode in ["Attack", "Special"]:
         self._p = self.damage + self.accuracy
-        self.essence = check(self.essence) # ONLY for atk and spatk!
+        self.essence = check(self.essence)
       elif fuzz.ratio(self.mode, "Spell") > 90:
         pass
       if self.properties:
         pass
     assert self._p <= moveMax, ExceedError
     self._p = max(self._p, 0)
+
+  def execute(self, user, target):
+    if self.mode in ["Attack", "Special"]:
+      if self.accuracy > target.stats.evade:
+        result = self.damage
+        result *= matchup(self.essence, target.essences)
+        if self.essence in user.essences:
+          result *= Fraction(3/2)
+        if self.mode == "Attack":
+          result *= user.stats.attack / target.stats.defense
+        elif self.mode == "Special":
+          result *= user.stats.special ** 2 / target.stats.resist ** 2
+        return int(result)
+      else:
+        return "Miss"
+    elif self.mode == "Spell":
+      pass
