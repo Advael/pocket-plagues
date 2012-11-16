165fd85e8757b27b63b59e850e6500a253f63e10
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-11-15 19:17:57 -0800

    okay. so, need to finish up logic for loading organisms/trainers. next level of abstraction is species which is the abstract, changing genome in the environment in game, but that should produce a single organism for e.g. a combat instance

--- organism.py
+++ organism.py
@@ -6,10 +6,20 @@ import control
 class organism:
   def __init__(self, jsonfile, control=None):
     self.__dict__ = load(open(jsonfile))
-    self.combat = combatant(self.combat)
-    self.essences = self.combat.essences
+    if self.combat:
+      self.combat = combatant(self.combat)
+    elif self.genome:
+      self.combat = combatant(phenotype(self.genome))
+      # do more stuff
     self.tactic = control or _control(self.tactic)
 
+
+
+def phenotype(genome):
+  """
+    return the combat phenotype from IV and EV based values
+  """
+  pass
 
 def _control(tactic):
   return control.keys[tactic]()
